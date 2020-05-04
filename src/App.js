7c2710b74e21cca0ee7fc164c713729d2a6986db
jonatandalbosco <48532656+jonatandalbosco@users.noreply.github.com>
2020-05-04 12:03:16 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import { Home } from './Home';
 import { About } from './About';
 import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
 
 
 
 function App() {
   return (
     <React.Fragment>
+      <NavigationBar/>
+      <Layout>
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route  path="/about" component={About} />
-            <Route  path="/contact" component={Contact} />
-            <Route  component={NoMatch} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
           </Switch>
         </Router>
-
+      </Layout>
 
 
 
